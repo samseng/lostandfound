739519d1dcc7c5043abc19713c83397435932b93
ernest55 <31727863+ernest55@users.noreply.github.com>
2017-11-14 20:20:20 +0900

    Add files via upload

---
+++ searchpage.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Lost & Found-Search Page</title>
+		<link href="searchpage.css" type="text/css" rel="stylesheet" />
+		<link href="img/favicon.png" type="image/png" rel="shortcut icon" />
+	</head>
+
+	<body>
+		<div class="searchbar">
+			<img src="img/smalllogo.png" alt="logo" />
+			<input class="searchbox" type="text" name="itemname" placeholder="Search for missing item" />
+			<input type="submit" value="Go!" style="width:100px;"/>
+		</div>
+
+		<div class="banner">
+			<img src="img/logo.png" alt="logo" />
+			<h1>Lost & Found</h1>
+			<h6>ONLINE CENTER</h6>
+		</div>
+
+	<div class="overall">
+
+	<div class="categories">
+		<h1>Categories</h1>
+		<ul>
+    		<li><a href="">Electronics</a></li>
+    		<br/>
+    		<li><a href="">Bags</a></li>
+    		<br/>
+    		<li><a href="">Books</a></li>
+    		<br/>
+    		<li><a href="">Others</a></li>
+		</ul>
+	</div>
+
+	<div class="display">
+
+		<div class="section1">
+			results for "keyword"
+		</div>
+
+		<div class="section2">
+		<div>
+			<img src="ex1.jpg" alt="ex1" />
+			<p>Item Name</p>
+			<p>Contact No.:</p>
+		</div>
+		<div>
+			<img src="ex2.jpg" alt="ex2" />
+			<p>Item Name</p>
+			<p>Contact No.:</p>
+		</div>
+		<div>
+			<img src="ex3.jpg" alt="ex3" />
+			<p>Item Name</p>
+			<p>Contact No.:</p>
+		</div>
+		</div>
+
+	</div>
+
+	<div class="pageno">
+		<p> (1-12) of 55 </p>
+	</div>
+
+	</body>
+
+	</html>
